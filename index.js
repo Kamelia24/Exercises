fe3be51ee444921bca6fbea6d3b0a1e47ed66b5b
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-04-07 16:09:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,86 @@
+function longestWord(str){
+    let words=str.split(" ");
+    longest="";
+    for(el in words){
+        if(longest.length<words[el].length){
+            longest=words[el];
+        }
+    }
+    console.log(longest)
+}
+longestWord("obelka dinq kutiq");
+
+ function firstThreeUpper(str){
+    let eddited=str.substr(0, 3).toUpperCase()+str.substr(3,);
+    console.log(eddited);
+ }
+ firstThreeUpper("kolko imam?");
+
+ function occurances(str,letter){
+     let howMany=0;
+     let sliced=str.split("");
+     for(let br in sliced){
+         if(sliced[br]==letter){
+             howMany++;
+         }
+     }
+     console.log(howMany);
+ }
+occurances('some test here, yoo', 'o');
+
+function primeOrNot(number){
+    let isPrime=true;
+    number=Number(number);
+    for(let i=2;i<number/2;i++){
+        if(number%i==0){isPrime=false;}
+    }
+    console.log(isPrime);
+}
+primeOrNot("10");
+
+function sortArray(arr){
+//arr=Number(arr);
+    for(let i=0;i<arr.length;i++){
+        for(let f=i+1;f<arr.length;f++){
+            if(arr[i]>arr[f]){
+                let swap=arr[i];
+                arr[i]=arr[f];
+                arr[f]=swap;
+            }
+        }
+    }
+    let output="";
+    for(br in arr){
+        output+=arr[br]+" ";
+    }
+    console.log(output)
+}
+sortArray([-3,8,7,6,5,-4,3,1,2])
+
+/*function time(){
+    let d=new Date();
+    
+        output=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+        console.log(output);
+    
+}
+time();*/
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s;
+    if(s!=today.getSeconds){
+    s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var output=h + ":" + m + ":" + s;
+    console.log(output);
+    var t = setTimeout(startTime, 500);
+}
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  startTime();
