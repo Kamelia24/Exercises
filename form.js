8b165ff98b8a434aa9e823bff89d054d900b9344
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-04-14 13:35:53 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
 $('#my_form').on('submit',function(event){
     event.preventDefault();
-    var name = $("input#first").val();
+    var name = $("input#first_name").val();
     var subject = $("input#last").val();
     var email = $("input#mail").val();
     var message = $("input#phnumber").val();
-
     var dataString = 'email=' + email + '&message=' + message + '&subject=' + subject + '&name=' + name ; 
 console.log("ok")
     $.ajax({
+        
         url: "http://test.zoiper.com/en/contact",
+        beforeSend : function (xhr) {
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        },
         type:   'POST',
         data: dataString,
         success: function(msg){
-            $("#successfull").css("visibility", "visible");
+            console.log(msg);
+            for(var key in msg){
+                if(msg[key]=="Value is required"){
+                    $("#failed").css("visibility","visible");
+                    break;
+                }else{
+                    $("#successfull").css("visibility", "visible");
+                }
+            }
+            //$("#successfull").css("visibility", "visible");
         },
         error: function() {
             $("#failed").css("visibility","visible")
