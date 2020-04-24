6fd473a316d8c63618b3e868bf8cea5ee5e820e3
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-04-24 14:05:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const http = require('http');
-const fs = require('fs');
-let app = http.createServer((req, res) => {
-    res.json({
+const express = require('express');
+const app = express();
+app.get("/",(req, res) => {
+    
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write('<html><body><p>Go to /home or /contact ,please</p></body></html>');
+        res.end();
+
+});
+app.get("/contact",(req, res) => {
+   
+        res.json({
         "visitor_type": "Value is required",
         "first_name": "Value is required",
         "email": "Value is required",
         "country": "Value is required",
         "subscribe": "Value is required",
         "typeofinq": "Value is required and can not be empty"
-    })
+        });
+    
 });
-app.listen(2000);
-console.log('Node server running on port 2000');
+app.get("/home",(req, res) => {
+    
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write('<html><body><p>This is the homepage. </p></body></html>');
+        res.end();
+    
+});
+var port=3000;
+app.listen(port,()=>{console.log(`Node JS API is listening on port: ${port}`);});
