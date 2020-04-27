c74659306133398029caf2657434e48e63a339cc
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-04-27 14:34:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const bodyParser= require("body-parser");
+const { check, validationResult, matchedData } = require('express-validator');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.all('/contact', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next()
+      });
+app.all('/home', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next()
+      });
+app.all('/', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next()
+      });
 app.get("/",(req, res) => {
     
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
@@ -7,8 +26,7 @@ app.get("/",(req, res) => {
         res.end();
 
 });
-app.get("/contact",(req, res) => {
-   
+/*app.post("/contact",(req, res) => {
         res.json({
         "visitor_type": "Value is required",
         "first_name": "Value is required",
@@ -18,7 +36,33 @@ app.get("/contact",(req, res) => {
         "typeofinq": "Value is required and can not be empty"
         });
     
-});
+});*/
+app.post('/contact', [
+        check('visitor_type')
+          .isIn(["company","private_person"])
+          .withMessage('Value is required'),
+        check('first_name')
+          .isLength({ min: 1 })
+          .withMessage('Value is required'),
+        check('email')
+          .isEmail()
+          .withMessage('This is not valid email address'),
+        check('country')
+          .isIn(["Bulgaria","Austria"])
+          .withMessage('Value is required'),
+        check('subscribe')
+          .isIn(["Zoiper for Desktop (windows, mac, linux)","Zoiper for Android","Zoiper for iOS"])
+          .withMessage('Value is required'),
+        check('typeofinq')
+          .equals(["sales"])
+          .withMessage('Value is required and can not be empty')
+      ], (req, res) => {
+        const errors = validationResult(req);
+        res.render('contact', {
+          data: req.body,
+          errors: errors.mapped()
+        });
+      });
 app.get("/home",(req, res) => {
     
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
